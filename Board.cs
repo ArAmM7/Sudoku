8340e91f0d8bb6d0cd52123839fb4ab9ad4c403a
Aram Manoukian <aramm97@gmail.com>
2017-08-02 22:39:25 +0400

    beta 1
    the program can solve a sudoku puzzle that I wrote in it, will implement a solution to input the puzzle from the console.

--- Board.cs
+++ Board.cs
@@ -10,16 +10,13 @@ namespace Sudoku
     {
         private int[,] a = new int[9, 9];
         private int[] check = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
-        public Board()
-        {
-
-        }
+        public Board() { }
         public void Display()
         {
             for (int i = 0; i < a.GetLength(0); i++)
             {
                 for (int j = 0; j < a.GetLength(1); j++)
-                    Console.Write(a[i, j] + " ");
+                    Console.Write(a[j, i] + " ");
                 Console.WriteLine();
             }
         }
@@ -27,6 +24,45 @@ namespace Sudoku
         {
             get => a[x, y];
             set => a[x, y] = value > 0 && value < 10 ? value : a[x, y];
+        }
+        public bool NotFull
+        {
+            get
+            {
+                foreach(var x in a)
+                {
+                    if (x == 0)
+                        return true;
+                }
+                return false;
+            }
+        }
+        public void Check(int x, int y)
+        {
+            List<int> t = new List<int> ();
+            for(int i=1;i<10;i++)
+            {
+                t.Add(i);
+            }
+            int c1 = x >= 0 && x < 3 ? 0 : x >= 3 && x < 6 ? 3 : 6;
+            int c2 = y >= 0 && y < 3 ? 0 : y >= 3 && y < 6 ? 3 : 6;
+
+            for (int i = c1; i < c1 + 3; i++)
+            {
+                for (int j = c2; j < c2 + 3; j++)
+                {
+                    t.Remove(a[i, j]);
+                }
+            }
+            for (int i = 0; i < 9; i++)
+            {
+                t.Remove(a[x, i]);
+                t.Remove(a[i, y]);
+            }
+            if (t.Count == 1)
+            {
+                a[x, y] = t[0];
+            }
         }
     }
 }
