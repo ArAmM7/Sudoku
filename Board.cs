a2ae04649e29f0245009709fc0c6fd07cb1b64cd
Aram Manoukian <aramm97@gmail.com>
2017-08-04 19:32:14 +0400

    beta 2. Added support for inputting from the console, added some error checking for the puzzle, will work on exception handling in the future

--- Board.cs
+++ Board.cs
@@ -8,28 +8,22 @@ namespace Sudoku
 {
     class Board
     {
-        private int[,] a = new int[9, 9];
-        private int[] check = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
-        public Board() { }
-        public void Display()
+        private int[,] a;
+
+        private int steps;
+
+        private int changes;
+
+        public int Steps
         {
-            for (int i = 0; i < a.GetLength(0); i++)
-            {
-                for (int j = 0; j < a.GetLength(1); j++)
-                    Console.Write(a[j, i] + " ");
-                Console.WriteLine();
-            }
+            get => steps;
         }
-        public int this[int x, int y]
-        {
-            get => a[x, y];
-            set => a[x, y] = value > 0 && value < 10 ? value : a[x, y];
-        }
-        public bool NotFull
+
+        private bool NotFull
         {
             get
             {
-                foreach(var x in a)
+                foreach (var x in a)
                 {
                     if (x == 0)
                         return true;
@@ -37,7 +31,131 @@ namespace Sudoku
                 return false;
             }
         }
-        public void Check(int x, int y)
+
+        public Board()
+        {
+            a = new int[9, 9];
+            steps = 0;
+            changes = 0;
+        }
+
+        public void Display()
+        {
+            for (int i = 0; i < a.GetLength(0); i++)
+            {
+                for (int j = 0; j < a.GetLength(1); j++)
+                    Console.Write(a[i, j] + " ");
+                Console.WriteLine();
+            }
+        }
+
+        public void Start()
+        {
+            Console.WriteLine("enter the sudoku puzzle from the keyboard one row at a time using space to move to the next column, and enter to move to the next row \n P.S. use zero for empty slots");
+            for (int i = 0; i < 9; i++)
+            {
+                string s = Console.ReadLine();
+                for (int j = 0, k = 0; j < 9; j++, k += 2)
+                {
+                    a[i, j] = int.Parse(s[k].ToString());
+                }
+            }
+            //checking for errors in the puzzle
+
+            List<int> t = new List<int>();
+
+            //checking in 3*3 squares one by one
+
+            for (int i = 0; i < 7; i+=3)
+            {
+                for (int j = 0; j < 7; j+=3)
+                {
+                    t.Clear();
+                    for (int x = i; x < i + 3; x++) 
+                    {
+                        for (int y = j; y < j + 3; y++)
+                        {
+                            if (a[x,y]!=0)
+                            {
+                                t.Add(a[x, y]); 
+                            }
+                        }
+                    }
+                    if(HasRepetition(t))
+                    {
+                        Finish();
+                        return;
+                    }
+                }
+            }
+            //Console.WriteLine("passed check 1");
+
+            //checking in rows and columns
+
+            for (int i = 0; i < 9; i++) 
+            {
+                t.Clear();
+                for (int j = 0; j < 9; j++)
+                {
+                    if (a[i, j] != 0)
+                    {
+                        t.Add(a[i, j]);
+                    }
+                }
+                if (HasRepetition(t))
+                {
+                    Finish();
+                    return;
+                }
+            }
+            //Console.WriteLine("passed ceck 2");
+
+            for (int i = 0; i < 9; i++)
+            {
+                t.Clear();
+                for (int j = 0; j < 9; j++)
+                {
+                    if (a[j, i] != 0)
+                    {
+                        t.Add(a[j, i]);
+                    }
+                }
+                if (HasRepetition(t))
+                {
+                    Finish();
+                    return;
+                }
+            }
+            //Console.WriteLine("passed check 3");
+            Solve();
+            Finish();
+        }
+
+        private void Solve()
+        {
+            while (NotFull)
+            {
+                changes = 0;
+                for (int i = 0; i < 9; i++)
+                {
+                    for (int j = 0; j < 9; j++)
+                    {
+                        if (a[i, j] == 0)
+                        {
+                            Check(i, j);
+                            steps++;
+                        }
+                    }
+                }
+                if(changes == 0 )//no changes after one full loop on the board, means that the puzzle can't be solved(any further)
+                {
+                    return;
+                }
+            }
+            //Console.WriteLine("solved");
+        }
+        
+        private void Check(int x, int y)
         {
             List<int> t = new List<int> ();
             for(int i=1;i<10;i++)
@@ -62,7 +180,49 @@ namespace Sudoku
             if (t.Count == 1)
             {
                 a[x, y] = t[0];
+                changes++;
             }
         }
+
+        private void Finish()
+        {
+            if (!NotFull)
+            {
+                Console.WriteLine("\n solved in {0} steps \n", Steps);
+                Display();
+                Console.ReadLine();
+            }
+            else
+            {
+                Console.WriteLine("\n puzzle can't be solved(any further or there are multiple solutions) after {0} steps or there are errors in the puzzle, check for errors or try another puzzle \n",Steps);
+                Display();
+                Console.ReadLine();
+            }
+        }
+
+        public bool HasRepetition(List<int> lst)
+        {
+            if (lst.Count > 1) 
+            {
+                int reps = 0;
+                foreach (int t1 in lst)
+                {
+                    foreach (int t2 in lst)
+                    {
+                        if (t1 == t2)////////////////////////////////////////////////////////////////////
+                        {
+                            reps++;
+                        }
+                    }
+                }
+                if (reps == lst.Count)
+                {
+                    return false;
+                }
+                return true; 
+            }
+            return false;
+        }
+
     }
 }
--- Program.cs
+++ Program.cs
@@ -11,67 +11,13 @@ namespace Sudoku
     {
         static void Main(string[] args)
         {
-            Board a = new Board();
-            {
-                a[0, 0] = 7;
-                a[1, 0] = 8;
-                a[2, 0] = 2;
-                a[7, 0] = 9;
-                a[8, 0] = 6;
-                a[0, 1] = 5;
-                a[1, 1] = 4;
-                a[1, 2] = 9;
-                a[2, 1] = 3;
-                a[7, 1] = 8;
-                a[8, 1] = 7;
-                a[0, 2] = 6;
-                a[2, 2] = 1;
-                a[6, 2] = 3;
-                a[8, 2] = 5;
-                a[3, 3] = 4;
-                a[6, 3] = 6;
-                a[7, 3] = 3;
-                a[3, 4] = 8;
-                a[4, 4] = 1;
-                a[5, 4] = 7;
-                a[6, 4] = 5;
-                a[8, 4] = 4;
-                a[3, 5] = 9;
-                a[5, 5] = 6;
-                a[7, 5] = 1;
-                a[8, 5] = 8;
-                a[1, 6] = 5;
-                a[2, 6] = 9;
-                a[3, 6] = 1;
-                a[4, 6] = 6;
-                a[2, 7] = 4;
-                a[3, 7] = 3;
-                a[4, 7] = 5;
-                a[5, 7] = 9;
-                a[0, 8] = 1;
-                a[1, 8] = 6;
-                a[2, 8] = 8;
-                a[4, 8] = 7;
-                a.Display();
-            }
-            int q = 0;
-            while (a.NotFull)
-            {
-                for (int i = 0; i < 9; i++)
-                {
-                    for (int j = 0; j < 9; j++)
-                    {
-                        if (a[i, j] == 0)
-                        {
-                            a.Check(i, j);
-                            q++;
-                        }
-                    }
-                }
-            }
-            Console.Beep();
-            Console.WriteLine(q);
-            a.Display();
+            Board x = new Board();
+            x.Start();
+            //List<int> lst = new List<int>();
+            //lst.Add(3);
+            //Console.WriteLine(x.HasRepetition(lst));
+
+
         }
     }
 }
